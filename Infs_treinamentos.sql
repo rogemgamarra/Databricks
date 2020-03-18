670bb079eea57b90a6c7432010cfac6e3c36c831
Rogério Martins Gamarra <62356179+rogemgamarra@users.noreply.github.com>
2020-03-18 18:27:26 -0300

    Add files via upload

---
+++ Infs_treinamentos.sql
@@ -0,0 +1,29 @@
+Engenheiro de Dados (Data Engineer) do time de Advanced Analytics (Diretoria de Innovation & Advanced Analytics) 
+responsável pela coordenação de projetos de dados para desenvolvimento do Data Lake da Cia e construção de algoritmos avançados.
+
+Main responsibilities
+Responsável pela especificação técnica dos projetos de dados
+Responsável pela gestão dos projetos de dados
+Responsável pela extração de dados dos sistemas origem
+Responsável pela carga de dados na nuvem
+Responsável pela organização do data lake da Companhia
+Responsável pela geração de visões analíticas
+Melhoria contínua da plataforma de dados
+Coordenação das demandas entre clientes internos e fornecedores
+
+Requirements And Skills
+Conhecimento de ferramentas de extração, transformação e carga de dados (ETL). Ex.: Pentaho Data Integration e Talend
+Conhecimento de bases de dados relacionais (MS SQL Server, PL/SQL) , não relacionais (MongoDB, Cassandra, etc) e de ferramentas de Big Data (HDFS, Hadoop, ferramentas análogas, de mercado ou open source)
+Pacote Office avançado
+Inglês avançado e espanhol desejável
+
+
+--Data science academy 
+1 - Data Lake - Design, projeto e integracao -- R$900,00
+2 - Design e Implementacao de Data Warehouses -- R$550,00
+3 - Machine Learning e IA em Ambientes Distribuidos -- R$800,00
+4 - Segurança e Alta Disponibilidade de dados -- R$600,00
+5 - Analytics, Visualizacao Relatorios -- R$460,00
+
+
+
